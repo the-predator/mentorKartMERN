acfe6f8620537b62707356479325f1b8a6940408
Vikas Negi <negivikas201@gmail.com>
2022-12-22 18:26:41 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+//js datatypes
+//Number
+var x = 10;
+console.log(typeof(x));
+//boolean
+var y = true;
+console.log(typeof(y));
+//unidentified
+var z;
+console.log(typeof(z));
+//null
+var a = null;
+console.log(typeof(a));
+//object
+var b = {
+     1 : 'a',
+     2 : 'b',
+     3 : 'c'
+        }
+//string
+var c = 'abc';
+console.log(typeof(c));
