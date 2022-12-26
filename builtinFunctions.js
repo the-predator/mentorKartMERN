5e93b18a7bd03fbd9809f76fe73688a29db0ed4b
Vikas Negi <negivikas201@gmail.com>
2022-12-26 17:38:49 +0530

    builtinfunction

---
+++ builtinFunctions.js
@@ -0,0 +1,12 @@
+//typecasting
+  //string to num
+  let a='82';
+  console.log('before typecasting :'+typeof(a));
+  a=Number.parseInt(a);
+  console.log('after typecasting :'+typeof(a));
+
+  //toString
+  let b = true;
+  console.log('before typecasting :'+typeof(b));
+  b = b.toString();
+  console.log('before typecasting :'+typeof(b));
