1cf2237966681d9c8667242c45c8aade441470e3
Vikas Negi <negivikas201@gmail.com>
2022-12-26 20:42:55 +0530

    array

---
+++ array.js
@@ -0,0 +1,40 @@
+const arr = ['abc','bcd','efg','jkl'];
+            //0,  , 1   , 2   ,  3  
+const zth = arr[0];
+const len = arr.length;
+console.log('zeroth element is '+ zth);
+console.log('length of array '+arr.length);
+for(let i=0; i<arr.length; i++)
+    {
+        console.log('pos -> '+ i +' element '+ arr[i]);
+    }
+    for(let i=0; i<arr.length; i++)
+    {
+        console.log('pos -> '+ i +' element '+ arr[i].toUpperCase());
+    }
+// here we go
+  const arr3=new Array();
+  arr3.push(1);
+  arr3.push(2);
+  arr3.push(3);
+  arr3.push(4);
+  arr3.push(5);
+  arr3.push(6);
+  arr3.push(7);
+  arr3.push(10);
+
+  for(let n=0; n<arr3.length;n++)
+    {
+        console.log('n = '+arr3[n]);
+    }
+
+    //find index of 10
+    for(let n=0; n<arr3.length; n++)
+      {
+        if(arr3[n]==10)
+          {
+            console.log('index of 10 is '+ n);
+            return n;
+          }
+      }
+    arr3.find();
