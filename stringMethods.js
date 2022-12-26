ea0311b163f4d5db94385276cf6c4cdc3c5b0900
Vikas Negi <negivikas201@gmail.com>
2022-12-26 17:56:58 +0530

    stringMethods

---
+++ stringMethods.js
@@ -0,0 +1,8 @@
+let str ="hlw there";
+
+console.log(str);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.repeat(3));
+console.log(str.split(''));
+console.log(str.substring(1,5));
