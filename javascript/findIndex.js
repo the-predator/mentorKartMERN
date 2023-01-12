5b7469f9b50cb6d9b3cd2bfb8f8a19fed180684c
Vikas Negi <negivikas201@gmail.com>
2023-01-12 17:40:55 +0530

    js: findIndex

--- javascript/findIndex.js
+++ javascript/findIndex.js
@@ -1,5 +1,7 @@
+//the following function returns if the element is greater tham 18
 const arr = [14, 13, 16, 18, 21, 44];
 function  check(age){
     return age >18;
 }
+//findIndex returns the index of the value in the array that is being passed through
 console.log('18+ is ' + arr.findIndex(check));
