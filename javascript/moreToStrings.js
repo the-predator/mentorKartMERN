f85ad2dc49366e8ff4208a52f95699e9c362444a
Vikas Negi <negivikas201@gmail.com>
2023-01-12 18:29:36 +0530

    string interpolation and concatination

--- javascript/moreToStrings.js
+++ javascript/moreToStrings.js
@@ -1,7 +1,9 @@
 const fNm = "abc";
 const lNm = "pqr";
+//string concatination using +
 const flNm = fNm + " " + lNm;
 console.log(flNm);
 
 // string interpolation
 const flNm2 =`my name is ${fNm} ${lNm}`;
+console.log(flNm2)
