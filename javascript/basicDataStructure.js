7f4f60dad3a4ad74286e77c4032f30654c1829cc
Vikas Negi <negivikas201@gmail.com>
2023-01-12 17:32:03 +0530

    js : basic DSA and filter method

--- javascript/basicDataStructure.js
+++ javascript/basicDataStructure.js
@@ -1,6 +1,9 @@
+// Array DS
 const arr = [1, 2, 4, 1, 2, 4];
 console.log(arr.includes(200));
 
+//filter method filters elements if they satisfy the condition
+//the following function removes  the duplicate items from an array
 const unqArr = arr.filter((el, ind, a) =>{
     var indOfCurrentItem = a.indexOf(el);
     return indOfCurrentItem == ind;
