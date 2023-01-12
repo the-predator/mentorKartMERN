f7369cfe5b299d3ff0bb518da0d301d3229364f2
Vikas Negi <negivikas201@gmail.com>
2023-01-12 17:29:03 +0530

    js : anonymous and arrow function

--- javascript/arrowFunction.js
+++ javascript/arrowFunction.js
@@ -5,18 +5,18 @@
          return n*n;
      }
      console.log(mksqr(10));
-//anonymous function : a function having no name
+//anonymous function : a function having no name (anonymous function)
 let ansqr = function(n)
             {
                 return n*n;
             }
     console.log(ansqr(20));
-//arrow function
+//arrow function (doesnt event require the keyword function)
      let arrw1 = (n)=>{
                   console.log(this);//points towrds something else
                   return n*n;
      }
      console.log(arrw1(30))
-//oneliner arrow
+//oneliner arrow (the function only returns a single line statement and has only one agrument)
      let arrw2 = n => n*n;
      console.log(arrw2(40));
