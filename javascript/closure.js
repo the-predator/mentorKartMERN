23a9264b145a9545245f11b7276baa5e90fafb38
Vikas Negi <negivikas201@gmail.com>
2023-01-12 17:35:35 +0530

    js : closure

--- javascript/closure.js
+++ javascript/closure.js
@@ -1,6 +1,6 @@
 //innerfunction  uses things of outer  function
 function linearData() {//outerFunction
-    const arr = [-3, -2, -1, 0, 1, 2, 3]
+    var arr = [-3, -2, -1, 0, 1, 2, 3]
     function plotData() {//innerFunction
     console.log(arr);
     }
