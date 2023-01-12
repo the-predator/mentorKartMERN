b553f6edfbdcbd5d13f23cc85ed105fd344baf87
Vikas Negi <negivikas201@gmail.com>
2023-01-12 17:45:06 +0530

    js : hi

--- javascript/hi.js
+++ javascript/hi.js
@@ -1,4 +1,5 @@
+//console.log is used to print something
 console.log("hey hi");
-var a=10;
-var b = 20;
+var a=10;// variable declaration and initialization
+var b = 20;// variable declaration and initialization
 console.log(a+b);
