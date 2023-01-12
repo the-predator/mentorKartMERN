bcb737355d9ccda38e7d8d516e7cf203fcada911
Vikas Negi <negivikas201@gmail.com>
2023-01-12 19:30:11 +0530

    mapReduceFilter

---
+++ javascript/mapReduceFilter.js
@@ -0,0 +1,22 @@
+//map
+const array1 = [1, 4, 9, 16];
+
+const map1 = array1.map(x => x * 2);
+console.log(map1);
+//map functionality using foreach
+const map2 = new Array();
+array1.forEach(el => map2.push(el*2));
+console.log(map2)
+
+//filter
+const arr = [1, 2, 3, 4 ,5 ,6 , 7, 8];
+const result = arr.filter(el => el > 6);
+console.log(result);
+//filter functionality using foreach
+const arr2 =  new Array();
+arr.forEach((el)=>{
+    if(el>6)arr2.push(el);
+})
+console.log(arr2);
+
+//reduce
