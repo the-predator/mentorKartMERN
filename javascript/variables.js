7bc4afb28c998fcc6d9910bd18960f7cba2d0972
Vikas Negi <negivikas201@gmail.com>
2023-01-12 18:38:49 +0530

    js : variables

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,15 +1,22 @@
+//var keyword has global scope
 var x = 5;
 var y = 6;
 var z = x + y;
+console.log(`${x} + ${y} = ${z}`);
 
-let x = 5;
-let y = 6;
-let z = x + y;
+//let has  local scope 
+let p = 5;
+let q = 6;
+let r = p + q;
+console.log(`${p} + ${q} = ${r}`);
 
 x = 5;
 y = 6;
 z = x + y;
+console.log(`${x} + ${y} = ${z}`);
 
-const x = 5;
-const y = 6;
-let z = x + y;
+//const has global scope and it cannot be edited
+const a = 5;
+const b = 6;
+let c = a + b;
+console.log(`${a} + ${b} = ${c}`);
